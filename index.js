224124de55b23272d71f3cab4588d14052720b91
Anastasya <shamr44@mail.ru>
2022-12-09 16:21:55 +0300

    add event

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const pet = document.querySelector('#pet');
 document.addEventListener('keydown', function(event) {
     jump();
 });
+document.addEventListener('click', function(event) {
+    jump();
+});
 
 function jump(){
     if(character.classList != 'jump'){
@@ -17,9 +20,7 @@ function jump(){
 let isAlive = setInterval(function(){
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('top'));
     let petLeft= parseInt(window.getComputedStyle(pet).getPropertyValue('left'));
-    console.log(characterTop);
-    console.log(petLeft);
-    if(petLeft < 120 && petLeft > 50 && characterTop >=300)
+    if(petLeft < 90 && petLeft > 20 && characterTop >=300)
     {
         alert('GAME OVER!!!');
     }
